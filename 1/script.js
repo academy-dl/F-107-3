20c6bf6862d4d267a7fb28c837b892d2ac9e639e
Oleg Bugaichuk <olegussek@gmail.com>
2021-06-15 18:47:18 +0400

    1

---
+++ 1/script.js
@@ -0,0 +1,18 @@
+const name = prompt('Введите имя: ');
+const sName = prompt('Введите фамилию: ');
+const age = +prompt('Введите возраст: ');
+
+const user = {
+  name,
+  sName,
+  age
+};
+
+console.log(typeof user.age);
+
+// const str = prompt('Введите свое имя: ');
+
+// str += ' умный';
+
+// alert(str);
+
